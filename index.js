7af3f980dcff411069070ff17e178d5f5a3736b5
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2022-12-04 13:45:27 +0000

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(driverArr, nameSought){
+    return driverArr.filter(driver => driver.toLowerCase() === nameSought.toLowerCase());
+}
+
+function fuzzyMatch(driverArr, nameSought){
+    return driverArr.filter(driver => driver.startsWith(nameSought));
+}
+
+function matchName(driverObjsArr, nameSought){
+    return driverObjsArr.filter(driverObj => driverObj.name === nameSought);
+}
